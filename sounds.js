5ce592193bfa5825c6d53aef27acb2e5d1d3736f
lbCodesCanada <luka.bakic519@gmail.com>
2026-01-30 19:51:33 -0500

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -1,9 +1,9 @@
-// =======================================
-// AMG GAME ENGINE – sounds.js
-// Loads engine, horn, and signal sounds
-// =======================================
-
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+
+import { loadEngine } from "./sounds/Engine.js";
+import { loadTwinTurbo } from "./sounds/TwinTurbo.js";
+import { loadHorn } from "./sounds/Horn.js";
+import { loadSignals } from "./sounds/Signals.js";
 
 export function loadSounds(state, camera) {
 
@@ -12,35 +12,8 @@ export function loadSounds(state, camera) {
 
   const loader = new THREE.AudioLoader();
 
-  // Engine idle
-  state.engineIdle = new THREE.Audio(listener);
-  loader.load("./sounds/engine_idle.mp3", (buffer) => {
-    state.engineIdle.setBuffer(buffer);
-    state.engineIdle.setLoop(true);
-    state.engineIdle.setVolume(0.6);
-    state.engineIdle.play();
-  });
-
-  // Engine rev layer
-  state.engineRev = new THREE.Audio(listener);
-  loader.load("./sounds/engine_rev.mp3", (buffer) => {
-    state.engineRev.setBuffer(buffer);
-    state.engineRev.setLoop(true);
-    state.engineRev.setVolume(0);
-    state.engineRev.play();
-  });
-
-  // Horn
-  state.hornSound = new THREE.Audio(listener);
-  loader.load("./sounds/horn.mp3", (buffer) => {
-    state.hornSound.setBuffer(buffer);
-    state.hornSound.setVolume(1.0);
-  });
-
-  // Turn signal tick
-  state.signalTick = new THREE.Audio(listener);
-  loader.load("./sounds/signal_tick.mp3", (buffer) => {
-    state.signalTick.setBuffer(buffer);
-    state.signalTick.setVolume(0.7);
-  });
+  loadEngine(state, listener, loader);
+  loadTwinTurbo(state, listener, loader);
+  loadHorn(state, listener, loader);
+  loadSignals(state, listener, loader);
 }
